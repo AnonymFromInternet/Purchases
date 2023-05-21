b5db360ae0a2e46ed64731f70788dfcfcd00b984
arturkeil <kyleartur@gmail.com>
2023-05-21 23:47:06 +0500

    Temp

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Purchases
+# This is the repo for the App, which allows users to make online purchases
+### Uses Postgres, Stripe, microservice for the confirmation sending per email
---
+++ cmd/web/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+type config struct {
+	port int
+	env  string
+	api  string
+
+	db struct {
+		dataSourceName string
+	}
+
+	stripe struct {
+		secretKey string
+		publicKey string
+	}
+}
+
+type application struct {
+	config        config
+	infoLog       *log.Logger
+	errorLog      *log.Logger
+	templateCache map[string]*template.Template
+	version       string
+}
+
+func (application *application) serve() error {
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%d", application.config.port),
+		Handler:           application.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       5 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	application.infoLog.Println(fmt.Sprintf("Starting HTTP server in mode %s on port %d", application.config.env, application.config.port))
+
+	return server.ListenAndServe()
+}
+
+func main() {
+	var config config
+
+	flag.IntVar(&config.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&config.env, "env", "development", "Application environment{development|production}")
+	flag.StringVar(&config.api, "api", "http://localhost:4001", "URL to api")
+
+	flag.Parse()
+
+	config.stripe.publicKey = os.Getenv("STRIPE_PUBLIC_KEY")
+	config.stripe.secretKey = os.Getenv("STRIPE_SECRET_KEY")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	templateCache := make(map[string]*template.Template)
+
+	application := &application{
+		config:        config,
+		infoLog:       infoLog,
+		errorLog:      errorLog,
+		templateCache: templateCache,
+		version:       version,
+	}
+
+	err := application.serve()
+	if err != nil {
+		application.errorLog.Println("[main]:[main] - err := application.serve()", err)
+		log.Fatal(err)
+	}
+
+}
