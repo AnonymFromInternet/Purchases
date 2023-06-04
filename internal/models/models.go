654cef8a83aa4adf80c72c4ebc4141474f3def05
arturkeil <kyleartur@gmail.com>
2023-06-04 12:22:50 +0500

    Adding migrations, models for db

--- internal/models/models.go
+++ internal/models/models.go
@@ -20,16 +20,6 @@ func NewModels(db *sql.DB) Models {
 	}
 }
 
-type Widget struct {
-	Id             int       `json:"id"`
-	Name           string    `json:"name"`
-	Description    string    `json:"description"`
-	InventoryLevel int       `json:"inventoryLevel"`
-	Price          int       `json:"price"`
-	CreatedAt      time.Time `json:"-"`
-	UpdatedAt      time.Time `json:"-"`
-}
-
 func (model *DBModel) GetWidget(id int) (Widget, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
@@ -48,4 +38,62 @@ func (model *DBModel) GetWidget(id int) (Widget, error) {
 	}
 
 	return widget, nil
+}
+
+// Models for the postgres db
+
+type Widget struct {
+	Id             int       `json:"id"`
+	Name           string    `json:"name"`
+	Description    string    `json:"description"`
+	InventoryLevel int       `json:"inventoryLevel"`
+	Price          int       `json:"price"`
+	CreatedAt      time.Time `json:"-"`
+	UpdatedAt      time.Time `json:"-"`
+}
+
+type Order struct {
+	Id            int
+	WidgetIs      int
+	TransactionId string
+	StatusId      int
+	Quantity      int
+	Amount        int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+type Status struct {
+	Id        int
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type TransactionStatus struct {
+	Id        int
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Transaction struct {
+	Id                  int
+	Amount              int
+	Currency            string
+	LastFour            string
+	BackReturnCode      string
+	TransactionStatusId int
+	CreatedAt           time.Time
+	UpdatedAt           time.Time
+}
+
+type User struct {
+	Id        int
+	FirstName string
+	LastName  string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
