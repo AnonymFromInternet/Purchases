fd550c5b4ede9cd97c9b0730fca5dd3fb6517231
arturkeil <kyleartur@gmail.com>
2023-07-02 17:12:03 +0500

    Adding invoice microservice handlers

---
+++ cmd/microservices/invoice/helpers.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/AnonymFromInternet/Purchases/internal/contentTypes"
+	"github.com/AnonymFromInternet/Purchases/internal/models"
+	"io"
+	"net/http"
+)
+
+type AnswerPayload struct {
+	Error   bool          `json:"error"`
+	Message string        `json:"message"`
+	Token   *models.Token `json:"token"` // TODO: Зачем указатель?
+}
+
+// readJSONInto check if request size is no greater than maxSizeInBytes and decode payload from request into arg
+func (application *application) readJSONInto(ptrToData interface{}, w http.ResponseWriter, r *http.Request) {
+	maxSizeInBytes := 1048576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxSizeInBytes))
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(ptrToData)
+	if err != nil {
+		application.errorLog.Println("cannot decode data from payload", err)
+
+		return
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		application.errorLog.Println(errors.New("body must have only a single JSON value"))
+	}
+}
+
+func (application *application) convertToJsonAndSend(data interface{}, w http.ResponseWriter) {
+	output, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		application.errorLog.Println("cannot convert data into json", err)
+
+		return
+	}
+
+	w.Header().Set(contentTypes.ContentTypeKey, contentTypes.ApplicationJSON)
+	_, _ = w.Write(output)
+}
--- cmd/microservices/invoice/invoice-handlers.go
+++ cmd/microservices/invoice/invoice-handlers.go
@@ -3,11 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
+type Order struct {
+	ID        int       `json:"id"`
+	Quantity  int       `json:"quantity"`
+	Amount    int       `json:"amount"`
+	Product   string    `json:"product"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
 func (application *application) handlerGetCreateAndSendInvoice(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "microservice works")
-	if err != nil {
-		return
-	}
+	// receive json
+	var order Order
+	application.readJSONInto(&order, w, r)
+
+	// generate pdf with invoice data
+
+	// create mail
+
+	// send mail with attachment
+
+	// send response
+	var answerPayload AnswerPayload
+	answerPayload.Error = false
+	answerPayload.Message = fmt.Sprintf("Invoice %d.pdf created and sent to %s", order.ID, order.Email)
+	application.convertToJsonAndSend(answerPayload, w)
 }
